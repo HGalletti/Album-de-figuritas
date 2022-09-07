b3d9c9228f5c0bb0cbb8f7c42038e105aefc6aaa
Hernán Galletti <hgarster@gmail.com>
2022-09-07 13:32:32 -0300

    Código mejor escrito, documentación y un resumen de lo hallado en readme.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Simulación de llenado de album de figuritas de un mundial.
+# Simulación del llenado del álbum de figuritas del mundial Qatar 2022 de Panini.
+
+El álbum de figuritas del mundial tiene 670 figuritas distintas. La venta de estas se hace mediante sobres donde vienen 5. 
+
+Este programa imita la situación de una persona comprando sobres y pegando las figuritas del sobre en el album. Este programa mediante una cantidad predefinida de simulaciones permite estimas las respuestas de las siguientes preguntas:
+
+- Adquiridos una cantidad prefijada de sobres (por defecto 800), ¿qué probabilidad hay que se complete el album con las figuritas de esos sobres?
+
+- ¿Cuál es el valor esperado y desviación estándar del número de sobres necesarios para completar el album? 
+
+Además, con las simulaciones hechas graficamos el histograma de la cantidad de sobres necesarios para completar el álbum.
+
+##Se hacen algunas suposiciones: 
+
+-Todas las figuritas son equiprobables. 
+-La elección de las 5 figuritas que vienen en cada sobre es con reposición (por lo tanto, en un sobre puede haber figuritas repetidas).
+-Para completar el album siempre suponemos que no hay intercambios con otros coleccionistas de figuritas.
+
+##Resultados hallados en una corrida del programa:
+
+ La probabilidad estimada de llenar el album con 800 sobres es de 15.2  %. (1000 simulaciones).
+ 
+ Por otro lado, si la cantidad de sobres es ilimitada, el histograma para la cantidad de sobres para llenar el album es:
+  
+<p align = "center">
+  <img src = "Figuritas.png" width = 500>
+</p>
+(Obtenido mediante 5000 simulaciones).
+
+ La cantidad de sobres esperados para completar el album es  951.7066 con una desviación estándar de 169.0872
+
+Los cuartiles para la cantidad de sobres necesarios son los siguientes:
+
+ quantile(x)
+  0%  25%  50%  75% 100% 
+ 595  834  922 1038 2014
+ 
--- albumfiguritas.R
+++ albumfiguritas.R
@@ -1,19 +1,20 @@
+
 rm(list=ls())
 
 cant_figuritas=670
-n=1000 #cantidad de simulaciones con sobres fijos
-m=500 #cantidad de simulaciones de cuantos sobres
-n_sobres=800
+n=1000 #cantidad de simulaciones con cantidad de sobres fijos (n_sobres)
+m=5000 #cantidad de simulaciones con cantidad de sobres ilimitada.
+n_sobres=800 #cantidad de sobres fijos
 
-#Funciones
-pegar_sobre = function(a,p){
+#Funciones:
+pegar_sobre = function(a,p){ #Esta función simula la acción de abrir un paquete de figuritas y pegarlas en el album
   for (j in 1:5) {
     a[p[j]]=TRUE
   }
   return(a)
 }
 
-album_lleno = function(album){
+album_lleno = function(album){ #Devuelve True si el album está lleno y False si no.
   salida= FALSE
   if (all(album)==1) {
     salida=TRUE
@@ -21,7 +22,7 @@ album_lleno = function(album){
   return(salida)
 }
 
-probaconnsobres= function(n, n_sobres){
+probaconnsobres= function(n, n_sobres){ #Estima la probabilidad de completar el album con la cantidad prefijada de sobre n_sobres.
   exitos=0
   for (k in 1:n) {
     album=rep(FALSE, cant_figuritas)
@@ -33,15 +34,14 @@ probaconnsobres= function(n, n_sobres){
       exitos=exitos+1
     }
   }
-  print('La probabilidad estimada de llenar el album con la cantidad de sobres propuesta es ')
-  print(exitos/n)
+  cat('La probabilidad estimada de llenar el album con ', n_sobres, ' sobres es de ', 100*exitos/n, ' %.')
 }
 
-cuantossobres=function(cant_figuritas){
+cuantossobres=function(cant_figuritas){ #Esta simulación cuenta la cantidad de sobres que hacen falta hasta llenar el album desde 0.
   album=rep(FALSE, cant_figuritas)
   sobres=0
   while (album_lleno(album)!=1) {
-    paquete=sample (1:670,5, replace=TRUE) 
+    paquete=sample (1:cant_figuritas,5, replace=TRUE) 
     album=pegar_sobre(album,paquete)
     sobres=sobres+1
   }
@@ -51,16 +51,22 @@ cuantossobres=function(cant_figuritas){
 }
 
 
-
-
-
-#Ejecutar
+#Programa a ejecutar:
 probaconnsobres(n, n_sobres)
   #Gráfico de sobres:
 x=rep(0, m)
 for (i in 1:m){
 x[i]=cuantossobres(cant_figuritas)
+#x son los sobres necesarios para llenar el album, cada lugar de x es el resultado de una simulación distinta.
 }
-plot(x) #x son los sobres necesarios para llenar en album, en cada lugar una simulacion.
-hist(x)
+#plot(x) 
+hist(x,
+     main="Cantidad de sobres hasta completar el album",
+     xlab="Cantidad de sobres",
+     col="lightblue",
+     breaks=30,
+     freq=TRUE
+)
+cat('La cantidad de sobres esperados para completar el album es ', mean(x), ' con una desviación estándar de ', sd(x))
+cat('Los cuantiles para la cantidad de sobres necesarios son los siguientes:')
 quantile(x)
